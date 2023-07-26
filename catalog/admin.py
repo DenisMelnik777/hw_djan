5bb9b5912c19238fc5009162f0756c69e8e10422
DenisMelnik777 <129406422+DenisMelnik777@users.noreply.github.com>
2023-07-26 15:51:13 +0300

    Add files via upload

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,2 +1,14 @@
 from django.contrib import admin
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'purchase_price',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'description',)
