e108dba4d90a74e04ac75bdb3ed7e58608866892
DenisMelnik777 <129406422+DenisMelnik777@users.noreply.github.com>
2023-07-28 11:54:21 +0300

    Add files via upload

---
+++ catalog/managment/commands/fill.py
@@ -0,0 +1,13 @@
+﻿from catalog.models import Product
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    products_data = [{'name': 'NZXT'}, {'description': ''},
+                     {'name': 'Deepcool'}, {'description': ''},
+                     {'name': 'Phantecs'}, {'description': ''},
+                     {'name': 'Termaltake'}, {'description': ''},
+                     {'name': 'Lenovo'}, {'description': ''},
+                     ]
+    for product_item in products_data:
+        Product.objects.create(**product_item)
